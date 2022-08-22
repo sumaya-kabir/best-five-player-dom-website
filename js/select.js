accfe87e53e3cdd0be93a214dc3dd212ec872a74
sumaya-kabir <sumayakabir4547@gmail.com>
2022-08-23 00:43:33 +0600

    btn disabled done

--- js/select.js
+++ js/select.js
@@ -1,8 +1,19 @@
 function addPlayer(element){
     const selectedFive = document.getElementById('selected-five');
-    const selectLi = document.createElement('li');
-    selectLi.innerText = element.parentNode.parentNode.children[0].innerText;
-    selectedFive.appendChild(selectLi);
+    const listLength = selectedFive.childNodes.length;
+
+    // show error if select more than 5
+    if(listLength > 5){
+        alert('Only Select Five');
+    } else {
+        const selectLi = document.createElement('li');
+        selectLi.innerText = element.parentNode.parentNode.children[0].innerText;
+        selectedFive.appendChild(selectLi);
+
+        // disabled button after click
+        element.setAttribute('disabled', true);
+        
+    }
 }
 
 
